396af701807960b239414fda52a60dd4cdeab0b9
Stefan Ober <StefanO@ppedv.de>
2026-02-19 11:24:44 +0100

    Write-Verbose , Write-Debug

--- Hilfreiches/Get-EventLog.ps1
+++ Hilfreiches/Get-EventLog.ps1
@@ -1,4 +1,5 @@
-﻿param(
+﻿[cmdletBinding()]
+param(
 [Parameter(Mandatory=$true)]
 [int]$EventId,
 
@@ -6,6 +7,11 @@
 
 [string]$Computername = "localhost"
 )
+Write-Verbose -Message "Verbose Ausgaben sind optionale Ausgabe"
+Write-Verbose -Message "Vor der Abfrage"
+Write-Verbose -Message "Es wurden folgende Werte verwendet EventID:$EventId Newest:$Newest Computername:$Computername"
+
+Write-Debug -Message "DebugHaltepunkt vor der Abfrage"
 
 Get-EventLog -LogName Security -ComputerName $Computername | Where-Object -FilterScript {$PSItem.EventID -eq $EventId} | Select-Object -First $Newest
 
