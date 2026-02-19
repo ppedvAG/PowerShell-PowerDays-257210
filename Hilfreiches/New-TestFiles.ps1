42dbe7afe697ee4a28523f06c3912882b56ff7e5
Stefan Ober <StefanO@ppedv.de>
2026-02-19 15:58:39 +0100

    Moegliche Lösung mit Functions

--- Hilfreiches/New-TestFiles.ps1
+++ Hilfreiches/New-TestFiles.ps1
@@ -23,6 +23,31 @@ param(
 
 [switch]$Force
 )
+#Funktionsdeklaration
+function New-TestFiles 
+{
+[cmdletBinding()]
+param(
+[ValidateScript({Test-Path -Path $PSItem -Pathtype Container})]
+[Parameter(Mandatory=$true)]
+[string]$Path,
+
+[ValidateRange(1,99)]
+[int]$FileCount = 9,
+
+[Validatelength(3,20)]
+[string]$Name = "File"
+)
+
+    for($i = 1; $i -le $FileCount; $i++)
+    {
+        $FileName = $Name + ("{0:D2}" -f $i) + ".txt"
+        New-Item -Path $Path -Name $FileName -ItemType File
+    }
+
+}
+#Ende Funktionsdeklaration
+
 
 #Prüfung ob Ordner vorhanden
 $TestFilesDirPath = Join-Path -Path $Path -ChildPath $Name
@@ -42,20 +67,24 @@ if(Test-Path -Path $TestFilesDirPath -PathType Container)
 
 $TestFilesDir = New-Item -ItemType Directory -Path $Path -Name $Name 
 
+New-TestFiles -Path $TestFilesDir.FullName -FileCount $FileCount
+<# Ersetzt durch Funktion
 for($i = 1; $i -le $FileCount; $i++)
 {
     $FileName = "File" + ("{0:D2}" -f $i) + ".txt"
     New-Item -Path $TestFilesDir.FullName -Name $FileName -ItemType File
-}
+}#>
 
 for($i = 1; $i -le $DirCount; $i++)
 {
     $DirName = "Dir" + ("{0:D2}" -f $i) 
     $subdir = New-Item -Path $TestFilesDir.FullName -Name $DirName -ItemType Directory
 
+    New-TestFiles -Path $subdir.FullName -Name "$DirName-File" -FileCount $FileCount
+    <# Ersetzt durch Funktion
     for($j = 1; $j -le $FileCount; $j++)
     {
         $FileName = "$DirName-File" + ("{0:D2}" -f $j) + ".txt"
         New-Item -Path $subdir.FullName -Name $FileName -ItemType File
-    }
+    }#>
 }
